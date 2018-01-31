6fbc4943f19102af4f5995359cc90437a4093ce1
Karime Arreguin <Karime@karimes-air.mobile.asu.edu>
2018-01-31 12:59:47 -0700

    assignment1add

--- A1/server.js
+++ A1/server.js
@@ -18,9 +18,25 @@ app.get("/add", function (req, res) {
 });
 
 app.get("/sub", function (req, res) {
-    res.send(req.query);
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a - b;
+    res.send(result.toString());
 });
 
+app.get("/mult", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a * b;
+    res.send(result.toString());
+});
+
+app.get("/div", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a / b;
+    res.send(result.toString());
+});
 app.use(methodOverride());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
